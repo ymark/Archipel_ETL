a191fb8ae262fa27480f56831fd68b1ec2b08704
Yannis Marketakis <yannismarketakis@gmail.com>
2024-02-13 14:31:22 +0200

    export author details from HAL

--- src/main/java/gr/forth/ics/isl/common/HALResources.java
+++ src/main/java/gr/forth/ics/isl/common/HALResources.java
@@ -2,7 +2,7 @@ package gr.forth.ics.isl.common;
 
 public class HALResources {
     /* HAL REST API resources */
-    public static final String NUMBER_OF_RESULTS="1000";
+    public static final int NUMBER_OF_RESULTS=5000;
     public static final String INRAP="inrap";
     public static final String HAL_REST_API_PREFIX="https://api.archives-ouvertes.fr/search/";
 //    public static final String HAL_REST_API_AUTHORS_PARAMETERS="?q=*%3A*&fl=authIdHal_s,authIdHal_i,authFullName_s,authIdFormPerson_s,authIdPerson_i,authFirstName_s,authLastName_s,authOrganismId_i,authOrganism_s,authUrl_s";
--- src/main/java/gr/forth/ics/isl/syntaxNormalizer/HalSyntaxNormalizer.java
+++ src/main/java/gr/forth/ics/isl/syntaxNormalizer/HalSyntaxNormalizer.java
@@ -29,29 +29,37 @@ public class HalSyntaxNormalizer {
     private void harvestData(String collectionName,String authorsFilename) throws IOException, ParserConfigurationException, TransformerException {
         List<HalAuthor> authorsCollection=new ArrayList<>();
         log.info("Harvest all resources from HAL under colletion '{}'",collectionName);
-        URL url=new URL(HALResources.HAL_REST_API_PREFIX+collectionName+"/"+HALResources.HAL_REST_API_AUTHORS_PARAMETERS);
-        log.debug("Hitting URL '{}'",url.toString());
-        HttpURLConnection con=(HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("User-Agent", "Mozilla/5.0");
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8));
-        String inputLine;
-        StringBuilder response = new StringBuilder();
+        String baseUrl=HALResources.HAL_REST_API_PREFIX+collectionName+"/"+HALResources.HAL_REST_API_AUTHORS_PARAMETERS+"&"+HALResources.ROWS+"="+HALResources.NUMBER_OF_RESULTS;
+        int start=0;
+        while(true) {
+            URL url = new URL(baseUrl+"&"+HALResources.START+"="+start);
+            log.debug("Hitting URL '{}'", url.toString());
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
 
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-        if(!response.isEmpty()) {
-            JSONObject jsonResultObject = new JSONObject(response.toString()).getJSONObject(HALResources.RESPONSE);
-            JSONArray resultsArray=jsonResultObject.getJSONArray(HALResources.DOCS);
-            if(!resultsArray.isEmpty()) {
-                for (int i = 0; i < resultsArray.length(); i++) {
-                    authorsCollection.addAll(this.identifyAuthors(resultsArray.getJSONObject(i)));
-                }
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
             }
-        }else{
-            log.error("Received empty result as response");
+            in.close();
+            if (!response.isEmpty()) {
+                JSONObject jsonResultObject = new JSONObject(response.toString()).getJSONObject(HALResources.RESPONSE);
+                JSONArray resultsArray = jsonResultObject.getJSONArray(HALResources.DOCS);
+                if (!resultsArray.isEmpty()) {
+                    for (int i = 0; i < resultsArray.length(); i++) {
+                        authorsCollection.addAll(this.identifyAuthors(resultsArray.getJSONObject(i)));
+                    }
+                }else{
+                    break;
+                }
+            } else {
+                log.error("Received empty result as response");
+                break;
+            }
+            start+=HALResources.NUMBER_OF_RESULTS;
         }
         Document document=this.xmlifyAuthors(authorsCollection);
         Utils.exportXmlToFile(document,new File(authorsFilename));
@@ -69,26 +77,28 @@ public class HalSyntaxNormalizer {
 
     Collection<HalAuthor> identifyAuthors(JSONObject authorsJsonObject){
         Map<String,HalAuthor> retMap=new HashMap<>();
-        JSONArray authorsCompactForm=authorsJsonObject.getJSONArray(HALResources.AUTH_IDHASPRIMARYSTRUCTURE_FS);
-        for(int i=0;i<authorsCompactForm.length();i++){
-            String authDetailsStr=authorsCompactForm.getString(i);
-            log.debug("parsing author details in compact form '{}'",authDetailsStr);
-            HalAuthor author=new HalAuthor();
-            author.setPersonIdInternal(authDetailsStr.substring(0,authDetailsStr.indexOf(HALResources.FACET_SEP)-1).split("-")[0]);
-            if(retMap.containsKey(author.getPersonIdInternal())){
-                HalAuthor existingAuthor=retMap.get(author.getPersonIdInternal());
-                existingAuthor.addOrganization(authDetailsStr.substring(authDetailsStr.indexOf(HALResources.JOIN_SEP)+HALResources.JOIN_SEP.length()+1,
-                                                               authDetailsStr.lastIndexOf(HALResources.FACET_SEP)-1),
-                                                authDetailsStr.substring(authDetailsStr.lastIndexOf(HALResources.FACET_SEP)+HALResources.FACET_SEP.length()+1));
+        if(authorsJsonObject.has(HALResources.AUTH_IDHASPRIMARYSTRUCTURE_FS)){
+            JSONArray authorsCompactForm = authorsJsonObject.getJSONArray(HALResources.AUTH_IDHASPRIMARYSTRUCTURE_FS);
+            for (int i = 0; i < authorsCompactForm.length(); i++) {
+                String authDetailsStr = authorsCompactForm.getString(i);
+                log.debug("parsing author details in compact form '{}'", authDetailsStr);
+                HalAuthor author = new HalAuthor();
+                author.setPersonIdInternal(authDetailsStr.substring(0, authDetailsStr.indexOf(HALResources.FACET_SEP) - 1).split("-")[0]);
+                if (retMap.containsKey(author.getPersonIdInternal())) {
+                    HalAuthor existingAuthor = retMap.get(author.getPersonIdInternal());
+                    existingAuthor.addOrganization(authDetailsStr.substring(authDetailsStr.indexOf(HALResources.JOIN_SEP) + HALResources.JOIN_SEP.length() + 1,
+                                    authDetailsStr.lastIndexOf(HALResources.FACET_SEP) - 1),
+                            authDetailsStr.substring(authDetailsStr.lastIndexOf(HALResources.FACET_SEP) + HALResources.FACET_SEP.length() + 1));
 
-            }else{
-                author.setPersonIdHal(authDetailsStr.substring(0,authDetailsStr.indexOf(HALResources.FACET_SEP)-1).split("-")[1]);
-                author.setAuthorName(authDetailsStr.substring(authDetailsStr.indexOf(HALResources.FACET_SEP)+HALResources.FACET_SEP.length()+1,
-                        authDetailsStr.indexOf(HALResources.JOIN_SEP)-1));
-                author.addOrganization(authDetailsStr.substring(authDetailsStr.indexOf(HALResources.JOIN_SEP)+HALResources.JOIN_SEP.length()+1,
-                                                        authDetailsStr.lastIndexOf(HALResources.FACET_SEP)-1),
-                                       authDetailsStr.substring(authDetailsStr.lastIndexOf(HALResources.FACET_SEP)+HALResources.FACET_SEP.length()+1));
-                retMap.put(author.getPersonIdInternal(),author);
+                } else {
+                    author.setPersonIdHal(authDetailsStr.substring(0, authDetailsStr.indexOf(HALResources.FACET_SEP) - 1).split("-")[1]);
+                    author.setAuthorName(authDetailsStr.substring(authDetailsStr.indexOf(HALResources.FACET_SEP) + HALResources.FACET_SEP.length() + 1,
+                            authDetailsStr.indexOf(HALResources.JOIN_SEP) - 1));
+                    author.addOrganization(authDetailsStr.substring(authDetailsStr.indexOf(HALResources.JOIN_SEP) + HALResources.JOIN_SEP.length() + 1,
+                                    authDetailsStr.lastIndexOf(HALResources.FACET_SEP) - 1),
+                            authDetailsStr.substring(authDetailsStr.lastIndexOf(HALResources.FACET_SEP) + HALResources.FACET_SEP.length() + 1));
+                    retMap.put(author.getPersonIdInternal(), author);
+                }
             }
         }
         return retMap.values();
